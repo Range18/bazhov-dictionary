b45a1d4f01b5140908b725f0a3a6300c75ee2d88
Arslan Ibragimov <a.z.ibragimov@tbank.ru>
2026-01-15 23:52:10 +0500

    Add tale image link

--- src/app/tale-images/rdo/tale-image.rdo.ts
+++ src/app/tale-images/rdo/tale-image.rdo.ts
@@ -2,6 +2,12 @@ import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class TaleImageRdo {
+    private static apiUrl = '';
+
+    static init(apiUrl: string) {
+        TaleImageRdo.apiUrl = apiUrl.replace(/\/+$/, '');
+    }
+
     @ApiProperty({ example: '7d3f6d6a-6d7f-4c1a-9e84-1cc0f7f20c2a' })
     @Expose()
     id: string;
@@ -9,4 +15,10 @@ export class TaleImageRdo {
     @ApiProperty({ example: 'a1b2c3d4e5f6g7h8.png', description: 'Stored filename in storage' })
     @Expose()
     filename: string;
+
+    @ApiProperty({ description: 'Link to get image source' })
+    @Expose()
+    get link(): string {
+        return `${TaleImageRdo.apiUrl}/api/tale-images/${this.id}/source`;
+    }
 }
--- src/core/configs/api.config.ts
+++ src/core/configs/api.config.ts
@@ -4,4 +4,5 @@ import "dotenv/config";
 export const apiConfig = {
   port: get('API_PORT').default('3000').asPortNumber(),
   apiKey: get('API_KEY').required().asString(),
+  url: get('API_URL').required().asString(),
 };
